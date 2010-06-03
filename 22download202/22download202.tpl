04522e3ef35f38ac3e07bc2cae466abc3a6ad783
Koen Bollen <meneer@koenbollen.nl>
2010-06-03 17:11:45 +0200

    Added html generator and template.

---
+++ 22download202/22download202.tpl
@@ -0,0 +1,26 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <title>22download202</title>
+        <link rel="stylesheet" type="text/css" media="screen" href="22download202.css" />
+        <script language="JavaScript" src="http://code.jquery.com/jquery-1.4.2.min.js"></script>
+        <script language="JavaScript">
+            <!--
+
+$(document).ready(function() {
+    $('.genre > .info').hide();
+    $('.genre > .name').click(function(e) {
+        $('~ .info', this).toggle();
+    });
+});
+            //-->
+        </script>
+    </head>
+    <body>
+
+{content}
+
+    </body>
+</html>
--- 22download202/html.py
+++ 22download202/html.py
@@ -1,10 +1,8 @@
-import json
-from cgi import escape
+from leech import get22tracks
 
 def main():
-    with open( "22tracks.json" ) as fp:
-        genres = json.load( fp )
-    with open( "22tracks.tpl" ) as fp:
+    genres = get22tracks()
+    with open( "22download202.tpl" ) as fp:
         tpl = fp.read()
     content = u""
     for key in sorted(genres.keys()):
@@ -13,7 +11,13 @@ def main():
         for track in genres[key]:
             for k in ("title","artist","album"):
                 g += u"<span class='%s'>%s</span>\n" % (k,track[k])
-            g += u"<a class='url' href='%s'>download</a>\n" % escape(track['url'])
+            g += u"<a class='url' href='%s'>download</a>\n" % track['url']
         g += u"</div>\n</div>\n\n"
         content += g
     print tpl.replace( "{content}", content.encode( "utf-8" ) )
+
+if __name__ == "__main__":
+    main()
+
+# vim: expandtab shiftwidth=4 softtabstop=4 textwidth=79:
+
