4f53d5e683356d1383c079833860c59037d1f3cd
Koen Bollen <meneer@koenbollen.nl>
2010-06-03 17:33:25 +0200

    Changed the html generate, fix the with statement and added a div.

--- 22download202/html.py
+++ 22download202/html.py
@@ -2,17 +2,22 @@ from leech import get22tracks
 
 def main():
     genres = get22tracks()
-    with open( "22download202.tpl" ) as fp:
+    try:
+        fp = open( "22download202.tpl" )
         tpl = fp.read()
+    except (IOError, OSError):
+        tpl = "{content}"
     content = u""
     for key in sorted(genres.keys()):
         g = u"<div id='%s' class='genre'>\n<div class='name'>%s</div>\n"%(key, key.title())
         g += u"<div class='info'>\n"
         for track in genres[key]:
+            g += "<div class='track'>\n"
             for k in ("title","artist","album"):
                 g += u"<span class='%s'>%s</span>\n" % (k,track[k])
             g += u"<a class='url' href='%s'>download</a>\n" % track['url']
-        g += u"</div>\n</div>\n\n"
+            g += "</div>\n\n"
+        g += u"</div>\n</div>\n\n\n"
         content += g
     print tpl.replace( "{content}", content.encode( "utf-8" ) )
 
