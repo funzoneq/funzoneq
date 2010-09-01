f478b1a1ed7d2c736e9df653fdb8d8a53ad8b11f
Arnoud Vermeer <a.vermeer@freshway.biz>
2010-09-01 19:16:06 +0200

    World of Difference voter, omdat het KAN!

---
+++ worldofdifference/services.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+import logging, binascii, rc4, random, hashlib, names
+from pyamf.remoting.client import RemotingService
+
+key = 'wodrocks'
+
+def doVote(data):
+	appReferer = 'http://vodafone.egotribe.nl/WOD2010_Loader01.swf?random='+str(random.randint(10000, 99999))
+
+	logging.basicConfig(
+	    level=logging.DEBUG,
+	    format='%(asctime)s %(levelname)-5.5s [%(name)s] %(message)s'
+	)
+
+	gw = RemotingService('http://flashservices.egotribe.nl/gateway.php', referer=appReferer, logger=logging)
+	service = gw.getService('vodafoneGadget')
+
+	print service.saveVote(data)
+
+def randUserID():
+	x = str(random.randint(10000, 999999));
+	return hashlib.sha224(x).hexdigest()[:16]
+
+def genHyvesURL():
+	IPS = [ '94.100.114.165', '94.100.116.140', '94.100.114.70', '94.100.118.209', '94.100.120.1', '94.100.118.67', '94.100.114.22', '94.100.116.45', '94.100.124.174', '94.100.118.216', '94.100.124.38', '94.100.118.67', '94.100.118.109', '94.100.118.246', '94.100.114.79' ]
+	dir = str(random.randint(10000, 99999))
+	sub = random.randint(10000, 50000)
+	rest = sub % 100
+	rand = hashlib.sha224(str(rest)).hexdigest()[:4]
+
+	url  = "http://"
+	url += IPS[random.randint(0, len(IPS))-1]+"/"
+	url += dir+"00001-"+dir+"50000/"
+	url += dir+str(sub-rest+1)+"-"+dir+str((sub-rest)+100)+"/"
+	url += dir+str(sub)+"_14_"+rand+".jpeg"
+
+	return url
+
+def generateUserInfo():
+	data = 'efbbf92b7dab2f3e;'
+	data += randUserID()+';'
+	data += names.MName(3, "voor").New()+";"
+	data += names.MName(3, "achter").New()+";"
+	data += genHyvesURL()+";"
+
+	return data
+
+def hex0rcrypt(data, key):
+	crypt = rc4.rc4crypt(data, key)
+	return "z"+binascii.hexlify(crypt)
+
+
+for i in range(0, 20000):
+	data = generateUserInfo()
+	hex = hex0rcrypt(data, key)
+	doVote(hex)
+	sleep(0.5)
